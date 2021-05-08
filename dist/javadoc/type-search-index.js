61fd40f39b106b4a403899f75644d1ef1177b252
Jamshaid <jamshaidiqbal120@outlook.com>
2021-05-08 15:29:42 +0500

    added java doc

---
+++ dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ospi.park.library","l":"Book"},{"p":"ospi.park.library","l":"Crime"},{"p":"ospi.park.library","l":"Drama"},{"p":"ospi.park.library","l":"MainClass"}]
